bdad10c38bb4dbffeaf623e5729eb6a908504bbe
DanielMorales1103 <demoralesu@morurgt.com>
2024-05-17 19:50:06 -0600

    guardar memoria

--- Qlearning.py
+++ Qlearning.py
@@ -1,25 +1,41 @@
 import numpy as np
+import random
+import pickle
 
-def get_state(victorias, mano_ia):
-    """Genera una tupla que representa el estado actual del juego desde la perspectiva de la IA."""
-    estado = tuple((elemento, tuple(victorias["IA"][elemento])) for elemento in ['Fuego', 'Agua', 'Nieve'])
+Q = {}
+alpha = 0.5
+gamma = 0.9
+epsilon = 0.1
+
+def inicializar_Q():
+    global Q
+    try:
+        with open('q_table.pkl', 'rb') as f:
+            Q = pickle.load(f)
+    except FileNotFoundError:
+        Q = {}
+
+def guardar_q_table():
+    with open('q_table.pkl', 'wb') as f:
+        pickle.dump(Q, f)
+
+# Función obtener estado
+def get_state(victorias, mano_ia, mazo_user, mazo_ia, historial_acciones):
+    estado_ia = tuple((elemento, tuple(victorias["IA"][elemento])) for elemento in ['Fuego', 'Agua', 'Nieve'])
+    estado_user = tuple((elemento, tuple(victorias["User"][elemento])) for elemento in ['Fuego', 'Agua', 'Nieve'])
     cartas = tuple(sorted((carta.elemento, carta.numero) for carta in mano_ia))
-    return estado + cartas
+    num_cartas_restantes = (len(mazo_user), len(mazo_ia))
+    return estado_ia + estado_user + cartas + num_cartas_restantes + historial_acciones
+
+
 
 # Función de recompensa
 def reward(user_win, ia_win):
     if ia_win:
-        return 1  # Recompensa positiva si gana la IA
+        return 1  
     elif user_win:
-        return -1  # Recompensa negativa si gana el usuario
-    return -0.1  # Pequeña penalización por empate
-
-# Inicialización de la tabla Q
-Q = {}
-
-# Definición de parámetros de aprendizaje
-alpha = 0.5
-gamma = 0.9
+        return -1 
+    return -0.1 
 
 # Actualización de Q
 def update_Q(state, action, reward, next_state, all_possible_actions):
@@ -29,7 +45,7 @@ def update_Q(state, action, reward, next_state, all_possible_actions):
 
 # Selección de acción basada en Q
 def select_action(state, possible_actions):
-    if np.random.rand() < 0.1:  # Exploración con una probabilidad del 10%
+    if np.random.rand() < epsilon:  # Exploración con una probabilidad del 10%
         return np.random.choice(possible_actions)
     q_values = [Q.get((state, action), 0) for action in possible_actions]
     max_q = max(q_values)
